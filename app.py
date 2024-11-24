d26893f82150754895c6cdb46707998718214d52
hellopiyush0003 <hellopiyush0003@hotmail.com>
2024-11-24 10:29:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import pandas as pd
 from huggingface_hub import HfApi
 from sqlalchemy import create_engine, text
 from sqlalchemy.pool import NullPool
-
+print(os.getenv("GHP_TOKEN"))
 # Start a virtual display
 display = Display(visible=0, size=(1920, 1080))
 display.start()
